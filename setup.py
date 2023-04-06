424f2c4ec38f2d1b02c01f875d5059c96a23b46b
Martin Olveyra <molveyra@gmail.com>
2023-04-06 10:24:36 -0300

    update to version 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'e-models',
-    version      = '0.4',
+    version      = '1.0',
     description  = 'Tools for helping build of extraction models with scrapy spiders.',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
