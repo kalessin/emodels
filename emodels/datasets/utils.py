4e41aed55f310a60ec6c20d8050af2d37ca43515
Martin Olveyra <molveyra@gmail.com>
2023-08-03 19:34:28 -0300

    moved huggingface dependent code to a separate module

--- emodels/datasets/utils.py
+++ emodels/datasets/utils.py
@@ -5,20 +5,14 @@ import abc
 import gzip
 import json
 import logging
-from functools import partial
 from random import random
-from typing import List, Literal, Tuple, Protocol, cast, Dict, Any, IO, Optional, TypedDict
+from typing import List, Literal, Tuple, Protocol, cast, Dict, Any, IO, TypedDict
 
 from typing_extensions import Self
 from scrapy.http import TextResponse
 import lxml.html
 
-try:
-    from datasets import Dataset as HuggingFaceDataset, DatasetDict as HuggingFaceDatasetDict
-except ImportError:
-    pass
-
-from emodels.config import EMODELS_DIR, EMODELS_ITEMS_DIR
+from emodels.config import EMODELS_DIR
 from emodels.scrapyutils.response import ExtractTextResponse
 
 
@@ -108,64 +102,6 @@ class DatasetFilename(Filename):
         assert not self._file, "Already opened."
         with self.open("at") as fz:
             print(json.dumps(data), file=fz)
-
-
-class ItemsDatasetFilename(DatasetFilename):
-    @classmethod
-    def build_from_items(
-        cls,
-        name: str,
-        project: str,
-        classes: Optional[Tuple[str]] = None,
-        dataset_ratio: Tuple[float, ...] = DEFAULT_DATASET_RATIO,
-    ) -> Self:
-        """
-        Build a dataset dict from extracted items in user dataset folder.
-        - name is a name for the dataset. It will determine the storing filename.
-        - project is the name of the project the dataset belongs to. It will determine the storing filename.
-        - If classes is a tuple of strings, select only the specified
-        item subfolders.
-        - dataset_ratio is the same for get_random_dataset() and determines how samples are distributed
-          among train, test and validation buckets.
-        """
-        result = cls.local_by_name(name, project)
-        if os.path.exists(result):
-            raise ValueError(
-                "Output file already exists. "
-                f'open with {cls.__name__}.local_by_name("{name}", "{project}") or remove it for rebuilding'
-            )
-        for sf in os.listdir(EMODELS_ITEMS_DIR):
-            for f in os.listdir(os.path.join(EMODELS_ITEMS_DIR, sf)):
-                df = DatasetFilename(os.path.join(EMODELS_ITEMS_DIR, sf, f))
-                for sample in df:
-                    sample["dataset_bucket"] = get_random_dataset(dataset_ratio)
-                    result.append(sample)
-        return result
-
-    def to_hfdataset(self) -> HuggingFaceDatasetDict:
-        """
-        Convert to HuggingFace Dataset suitable for usage in transformers
-        """
-
-        def _generator(bucket: DatasetBucket):
-            for sample in self:
-                if sample["dataset_bucket"] != bucket:
-                    continue
-                for key, idx in sample["indexes"].items():
-                    if idx is None:
-                        continue
-                    yield {
-                        "markdown": sample["markdown"],
-                        "attribute": key,
-                        "start": idx[0],
-                        "end": idx[1],
-                    }
-
-        train = HuggingFaceDataset.from_generator(partial(_generator, "train"))
-        test = HuggingFaceDataset.from_generator(partial(_generator, "test"))
-
-        ds = HuggingFaceDatasetDict({"train": train, "test": test})
-        return ds
 
 
 class WebsiteSampleData(TypedDict):
