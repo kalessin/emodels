7d35450db75a4c4158e1017a937a68cc8b5d8f49
Martin Olveyra <molveyra@gmail.com>
2023-10-24 18:42:11 -0300

    update to version 1.6.8.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'e-models',
-    version      = '1.6.8',
+    version      = '1.6.8.1',
     description  = 'Tools for helping build of extraction models with scrapy spiders.',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
